b3629574bfd5ce9afa5b4efc8760b77b00e31bb8
unknown <muraliya@mcmaster.ca>
2024-11-27 19:40:55 -0500

    another

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.h"
+#include "objPos.h"
 
 
 Player::Player(GameMechs* thisGMRef)
@@ -7,12 +8,14 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos = objPos();
 }
 
 
 Player::~Player()
 {
     // delete any heap members here
+    //delete playerPos.pos; // i think this is  what i have to delete
 }
 
 objPos Player::getPlayerPos() const
